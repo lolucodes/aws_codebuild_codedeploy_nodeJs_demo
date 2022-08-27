b9badff7dea4e7a8edb9bfd3e1f97bec67f422a8
lolucodes <happycode20206@gmail.com>
2022-08-27 06:31:11 +0100

    v.2

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V2!')
+  res.send('CICD App V2!');
 })
 
 app.get('/status', (req, res) => {
-    res.send('ok')
+    res.send('ok');
   })
 
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
